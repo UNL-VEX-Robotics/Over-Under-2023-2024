68345e0b138e1aa4d2b00df6cecc514b3d0e709b
Minimac21 <mac.holland.d@gmail.com>
2024-02-28 17:50:06 -0600

    cleaner yet

--- over-under/include/auton.h
+++ over-under/include/auton.h
@@ -1,6 +1,5 @@
 void turn(double degrees);
 void go(double inches);
-void push(double inches);
 double convert(double degrees);
 
 void reset_positions();
@@ -9,6 +8,5 @@ void set_left_voltage(float voltage);
 void set_right_voltage(float voltage);
 void all_brake();
 
-void turn_absolute_debug(int degrees, int settled_margin, int integral_max_error, float Kp, float Ki, float Kd);
 void turn_right_relative_debug(int degrees, int settled_margin, int integral_max_error, float Kp, float Ki, float Kd);
 void turn_left_relative_debug(int degrees, int settled_margin, int integral_max_error, float Kp, float Ki, float Kd);
--- over-under/src/auton.cpp
+++ over-under/src/auton.cpp
@@ -79,7 +79,7 @@ void all_brake(){
     botRightDrive.brake();
 }
 
-void push(double inches){
+void go(double inches){
     double start = pros::millis();
     double now = pros::millis(); 
     double revolutions = inches / circum;
@@ -151,77 +151,6 @@ void push(double inches){
 
     }
 }
-
-void go(double inches){
-    double revolutions = inches / circum;
-    double encoder_units = 10.0/6.0 * revolutions * blue_ticks_per_rev;
-    reset_motors();
-    int i = 0;
-    double left_error = encoder_units;
-    double right_error = encoder_units;
-    double prev_left_error = left_error;
-    double prev_right_error = right_error;
-    double left_integral = 0;
-    double right_integral = 0;
-    double left_derivative = 0;
-    double right_derivative = 0;
-
-    double left_voltage = 0;
-    double right_voltage = 0;
-    while(( abs(left_error) > tick_margin ) && (abs(right_error) > tick_margin )){ 
-        prev_left_error = left_error;
-        prev_right_error = right_error;
-        left_error = encoder_units - (topLeftDrive.get_position() + midLeftDrive.get_position() + botLeftDrive.get_position()) / 3.0;
-        right_error = encoder_units - (topRightDrive.get_position() + midRightDrive.get_position() + botRightDrive.get_position()) / 3.0;
-
-        //if each iteration of the loop doesn't take a unifomidRightDrive amount of time, derivative and integral calculations need
-        //to consider time
-        left_derivative = left_error - prev_left_error;
-        right_derivative = right_error - prev_right_error;
-
-        if(abs(left_error) > integral_max_error_s){
-            left_integral = integral_max_error_s; 
-        } else {
-            left_integral = left_integral + Kis * left_error;
-        }
-        if(abs(right_error) > integral_max_error_s){
-            right_integral = integral_max_error_s;
-        } else {
-            right_integral = right_integral + Kis * right_error;
-        }
-        
-        left_voltage = Kps * left_error + Kis * left_integral + Kds * left_derivative;
-        right_voltage = Kps * right_error + Kis * right_integral + Kds * right_derivative;
-        if(left_voltage > 127){
-            double x = 127.0 * (i+100)/25000;
-            set_left_voltage(x > 127 ? 127 : x);
-        } else{
-            set_left_voltage(left_voltage);
-        }
-        if(right_voltage > 127) {
-            double x = 127.0 * (i+100)/25000;
-            set_right_voltage(x > 127 ? 127 : x);
-        } else {
-            set_right_voltage(right_voltage);
-        }
-
-        if(i % 5000 == 0){
-           
-            std::cout << "\n   L: ";
-            std::cout << left_error;
-            std::cout << "   R: ";
-            std::cout << right_error;
-            std::cout <<"\n";
-        }
-        i++;
-
-    }
-}
-
-
-//go but with loop control. Used to push into goal
-//we know we're in a "loop", i.e. bot is against the bars of the goal 
-//when standard deviation of previous x(10?) iterations' error drops below a certain threshold
 
 void turn_right_relative_debug(double degrees){
     double end_heading = degrees + imu.get_heading();
