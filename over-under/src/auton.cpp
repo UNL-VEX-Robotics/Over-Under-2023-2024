574f732e1954235960ea1b9638d85fbd3c8b8140
Minimac21 <mac.holland.d@gmail.com>
2024-04-22 12:03:49 -0500

    auton bug fix

--- over-under/src/auton.cpp
+++ over-under/src/auton.cpp
@@ -154,10 +154,10 @@ void turn_left_relative_debug(double degrees, PID turnPID){
     double error = degrees;
     double voltage = 0;
     turnPID.reset();
-    turnPID.setError(-degrees);
+    turnPID.setError(degrees);
     if(end_heading < 0){
-        while(imu.get_heading() > 350){
-            error = end_heading - imu.get_heading();
+        while(imu.get_heading() < 350){
+            error = imu.get_heading() -end_heading;
             voltage = turnPID.getNextValue(error);
             set_left_voltage(-voltage);
             set_right_voltage(voltage);
@@ -167,10 +167,16 @@ void turn_left_relative_debug(double degrees, PID turnPID){
             }
             i++;
         }
-        end_heading += 360;
         std::cout << "flipped to 0\n";
+        std::cout << imu.get_heading();
+        std::cout << "\n\tcurrent error: ";
+        std::cout << error;
+        end_heading+=360;
+        error = imu.get_heading() - end_heading;
+        std::cout << "\n\tcorrected error: ";
+        std::cout << error;
         while(!turnPID.isSettled()){
-            error = end_heading - imu.get_heading();
+            error = imu.get_heading() - end_heading;
             voltage = turnPID.getNextValue(error);
             set_left_voltage(-voltage);
             set_right_voltage(voltage);
@@ -183,7 +189,7 @@ void turn_left_relative_debug(double degrees, PID turnPID){
         return;
     } else {
         while(!turnPID.isSettled()){
-            error = end_heading - imu.get_heading();
+            error = imu.get_heading() - end_heading;
             voltage = turnPID.getNextValue(error);
             set_left_voltage(-voltage);
             set_right_voltage(voltage);
